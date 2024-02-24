851446ad815c382f8e6264ff02259639004c98b5
RuffledZest <vibhanshalok@gmail.com>
2024-02-24 16:02:23 +0530

    made the donorPage ui

---
+++ src/donorDash.jsx
@@ -0,0 +1,21 @@
+import NavbarDonor from "./components/navbarDonor";
+import StatusDonor from "./components/statusDonor";
+import TileDonor from "./components/tileDonor";
+
+
+
+
+function DonorDash() {
+    return (
+        <div>
+            <NavbarDonor/>
+            <StatusDonor/>
+            <TileDonor/>
+            
+            
+        </div>
+    )
+}
+
+
+export default DonorDash;
